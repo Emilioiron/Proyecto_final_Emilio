fb808fa954cac119117c3c9816e11acb4c831471
emilio <emilio@yahoo.es>
2020-04-23 01:07:45 +0200

    working

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-
 from p_acquisition import m_acquisition as mac
 from p_wrangling import m_wrangling as mwr
 from p_analysis import m_analysis as man
 from p_reporting import m_reporting as mre
 
+import argparse
 
-food = int(input('Introduce las raciones a comer: '))
-mre.reference_data(food)
-mre.insuline_admin()
+def main():
+    argument_parser()
 
-title_graphic = input('Enter the title: ')
-mac.acquire()
-Data = mwr.wrangle()
-by_hour_mean= mre.data_by_hour(Data)
-by_day_of_week_mean = mre.data_by_week(Data)
-mre.plotting_function(by_hour_mean,by_day_of_week_mean, title_graphic)
+def argument_parser():
+    parser = argparse.ArgumentParser(description = 'Set chart type or insline')
+    parser.add_argument("-b", "--bar", help="Produce a barplot", action="store_true")
+    parser.add_argument("-g", "--graph", help="Produce a graphic unique", action="store_true")
+    parser.add_argument("-c", "--calc", help="Calculate insulin needed", action="store_true")
+    args = parser.parse_args()
+    return args
+    #if args.bar:
+    #    print('Option barplot and lineplot are activates')
+    #if args.graph:
+    #    print('Option unique graphic activate')
+    #if args.calc:
+    #    print('Option insulin needed activate')
+    #return args
+
+    #food = int(input('Introduce las raciones a comer: '))
+
+    mre.reference_data(food)
+    #arguments = argument_parser()
+    mre.insuline_admin(args)
+
+
+    #title_graphic = input('Enter the title: ')
+    mac.acquire()
+    Data = mwr.wrangle()
+    by_hour_mean= mre.data_by_hour(Data)
+    by_day_of_week_mean = mre.data_by_week(Data)
+    mre.plotting_function(by_hour_mean,by_day_of_week_mean, title_graphic, args)
+    print('______________Pipeline successfully completed_______________')
+
+
 
 
 
 if __name__ == '__main__':
-    print('Pipeline successfully completed')
+    main()
+    food = int(input('Introduce las raciones a comer: '))
+    title_graphic = input('Enter the title: ')
+
+    #main(arguments)
+
 
 
 
--- p_reporting/m_reporting.py
+++ p_reporting/m_reporting.py
@@ -10,8 +10,6 @@ from datetime import date
 set_matplotlib_formats('svg')
 sns.set_style('whitegrid')
 
-#Data = pd.read_csv('./data/processed/Data.csv')
-
 def reference_data(food):
     new_food_dict = {'Unnamed: 0': {0: 0},
                      'Unnamed: 0.1': {0: 16},
@@ -38,17 +36,18 @@ def reference_data(food):
     new_food_dict['BWZ Carb Input (exchanges)'][0] = food
     new_food_dict['BWZ Food Estimate (U)'][0] = food
     new_food_today = pd.DataFrame(new_food_dict).to_csv('./data/results/new_food_today.csv')
-    return new_food_today
+    return
 
 
 
-def insuline_admin():
-    new_food_today = pd.read_csv('./data/results/new_food_today.csv')
-    model_pickle = joblib.load('./data/results/model.pkl')
-    administrar_insulina = model_pickle.predict(new_food_today)
-    print(f'Te debes administrar ' +
-          pd.DataFrame(administrar_insulina).iloc[0, 0].round(2).astype(str)
-          + ' unidades de insulina')
+def insuline_admin(args):
+    if args.calc == True:
+        new_food_today = pd.read_csv('./data/results/new_food_today.csv')
+        model_pickle = joblib.load('./data/results/model.pkl')
+        administrar_insulina = model_pickle.predict(new_food_today)
+        print(f'Te debes administrar '
+              + pd.DataFrame(administrar_insulina).iloc[0, 0].round(2).astype(str)
+              + ' unidades de insulina')
     return
 
 
@@ -64,61 +63,69 @@ def data_by_week(df):
     by_day_of_week_mean = by_day_of_week.groupby(['day_of_week']).sum()
     return   by_day_of_week_mean
 
-def plotting_function(df_hour,df_day, title):
+def plotting_function(df_hour,df_day, title, args):
+    if args.bar == True:
+        fig, ax = plt.subplots(2, 2,figsize=(4, 4), constrained_layout=True)
+        plt.suptitle(title, fontsize=25)
+        ax1 = plt.subplot2grid((2, 2), (0, 0))
+        ax2 = plt.subplot2grid((2, 2), (1, 0))
+        ax3 = plt.subplot2grid((2, 2), (0, 1))
+        ax4 = plt.subplot2grid((2, 2), (1, 1))
 
-    fig, ax = plt.subplots(2, 2,figsize=(4, 4), constrained_layout=True)
-    plt.suptitle(title, fontsize=25)
-    ax1 = plt.subplot2grid((2, 2), (0, 0))
-    ax2 = plt.subplot2grid((2, 2), (1, 0))
-    ax3 = plt.subplot2grid((2, 2), (0, 1))
-    ax4 = plt.subplot2grid((2, 2), (1, 1))
+        x = df_hour.index.astype(str).tolist()
 
-    x = df_hour.index.astype(str).tolist()
+        #plt.subplot(221)
+        #plt.grid(True)
+        ax1.bar(x, df_hour['BWZ Carb Input (exchanges)'], color = 'blue')
+        #ax1.set_xticklabels(ax2.get_xticklabels(all))
+        ax1.set_xlabel('Hour')
+        ax1.set_ylabel('Carb Input.U insulin')
+        #ax1.set_title(title, fontsize=20)
+        plt.tight_layout()
+        #plt.colorbar(pos, ax=ax1)
 
-    #plt.subplot(221)
-    #plt.grid(True)
-    ax1.bar(x, df_hour['BWZ Carb Input (exchanges)'], color = 'blue')
-    #ax1.set_xticklabels(ax2.get_xticklabels(all))
-    ax1.set_xlabel('Hour')
-    ax1.set_ylabel('Carb Input.U insulin')
-    #ax1.set_title(title, fontsize=20)
-    plt.tight_layout()
-    #plt.colorbar(pos, ax=ax1)
+        #plt.subplot(222)
+        #plt.grid(True)
+        ax2.bar(x, df_hour['BWZ Estimate (U)'], color = 'olive' )
+        #ax2.set_xticklabels(ax2.get_xticklabels               ())
+        ax2.set_xlabel('Hour')
+        ax2.set_ylabel('BWZ Estimate (U)')
+        #ax2.set_title(title, fontsize=16)
+        plt.tight_layout()
 
-    #plt.subplot(222)
-    #plt.grid(True)
-    ax2.bar(x, df_hour['BWZ Estimate (U)'], color = 'olive' )
-    #ax2.set_xticklabels(ax2.get_xticklabels               ())
-    ax2.set_xlabel('Hour')
-    ax2.set_ylabel('BWZ Estimate (U)')
-    #ax2.set_title(title, fontsize=16)
-    plt.tight_layout()
+        #plt.subplot(223)
+        #plt.grid(True)
+        day_of_week = ['L','M','X', 'J','V','S','D']
+        ax3.plot( day_of_week, 'BWZ Carb Input (exchanges)',
+                data=df_day, marker='o', markerfacecolor='blue',
+                markersize=8, color='skyblue', linewidth=2)
+        ax3.set_xlabel('Day of week')
+        ax3.set_ylabel('BWZ Carb Input')
+        plt.tight_layout()
 
-    #plt.subplot(223)
-    #plt.grid(True)
-    day_of_week = ['L','M','X', 'J','V','S','D']
-    ax3.plot( day_of_week, 'BWZ Carb Input (exchanges)',
-              data=df_day, marker='o', markerfacecolor='blue',
-              markersize=8, color='skyblue', linewidth=2)
-    ax3.set_xlabel('Day of week')
-    ax3.set_ylabel('BWZ Carb Input')
-    plt.tight_layout()
+        #plt.subplot(224)
+        #plt.grid(True)
+        ax4.plot( day_of_week, 'BWZ Estimate (U)',
+                data=df_day, marker='o', color='olive', linewidth=2)
+        ax4.set_xlabel('Day of week')
+        ax4.set_ylabel('BWZ Estimate')
+        plt.tight_layout()
 
-    #plt.subplot(224)
-    #plt.grid(True)
-    ax4.plot( day_of_week, 'BWZ Estimate (U)',
-              data=df_day, marker='o', color='olive', linewidth=2)
-    ax4.set_xlabel('Day of week')
-    ax4.set_ylabel('BWZ Estimate')
-    plt.tight_layout()
+        # Adjust the subplot layout, because the logit one may take more space
+        # than usual, due to y-tick labels like "1 - 10^{-3}"
+        plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                        wspace=0.35)
+        plt.show()
+        fig.savefig('./data/results/' + title + '.pdf', format='pdf')
 
-    # Adjust the subplot layout, because the logit one may take more space
-    # than usual, due to y-tick labels like "1 - 10^{-3}"
-    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
-                       wspace=0.35)
+    elif args.graph == True:
 
-    plt.show()
-    fig.savefig('./data/results/' + title + '.pdf', format='pdf')
+        sns.set(style="whitegrid")  # Estilo trazado
+
+        fig = plt.figure(figsize=(8, 4))
+        ax = sns.barplot(x='hour', y='BWZ Carb Input (exchanges)', data=df_hour)
+        plt.show()
+        fig.savefig('./data/results/' + title + '.pdf', format='pdf')
 
     return 
 
